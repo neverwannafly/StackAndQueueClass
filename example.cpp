9eeb883cd1f977eb5543817ecd626f6f235764cd
Shubham Anand <shubham_chess@live.com>
2018-05-05 23:11:13 +0530

    change comment to something respectful xD

--- example.cpp
+++ example.cpp
@@ -18,7 +18,9 @@
 bool isCharacterNumber(std::string character) {
 	bool condition = false;
 	char numberArray[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-	
+
+	// Let's assume the worst xD!
+
 	for (int i=0; i<character.size(); i++) {
 		for(int j=0; j<=9; j++) {
 			condition &= character[i]==numberArray[j];
